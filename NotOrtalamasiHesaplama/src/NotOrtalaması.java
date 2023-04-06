28d9beb335a4ff4b3aa4f2540d9334799d0ffa78
Sena <125875987+senaemirmusaogullari@users.noreply.github.com>
2023-04-06 15:36:52 +0300

    Not Ortalaması Hesaplama

---
+++ NotOrtalamasiHesaplama/src/NotOrtalaması.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class NotOrtalaması {
+
+	public static void main(String[] args) {
+
+		int mat, fizik, kimya, turkce, tarih, muzik;
+		Scanner inp = new Scanner(System.in);
+
+		System.out.print("Matematik Notunuz :");
+		mat = inp.nextInt();
+
+		System.out.print("Fizik Notunuz :");
+		fizik = inp.nextInt();
+
+		System.out.print("Kimya Notunuz :");
+		kimya = inp.nextInt();
+
+		System.out.print("Türkçe Notunuz :");
+		turkce = inp.nextInt();
+
+		System.out.print("Tarih Notunuz :");
+		tarih = inp.nextInt();
+
+		System.out.print("Müzik Notunuz :");
+		muzik = inp.nextInt();
+
+		int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+		double sonuc = toplam / 6.0;
+		System.out.println("Ortalamanız :" + sonuc);
+
+		if (sonuc > 60) {
+			System.out.println("Sınıfı Geçti. ");
+		} else if (sonuc < 60) {
+			System.out.println("Sınıfta Kaldı. ");
+
+		}
+	}
+
+}
